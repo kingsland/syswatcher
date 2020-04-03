ecaf0fdab1053d7c7f2c049394636a5d4585b6d4
shijiayu <shijiayu@redmine.com>
2020-04-03 14:04:03 +0800

    libcpu: modify data storage space.

--- plugins/cpu/libcpu.c
+++ plugins/cpu/libcpu.c
@@ -67,7 +67,7 @@ int send_data(item_t *data);
 int cpu_data_collect(item_t *data)
 {
     char *p;
-    int data_idx = 0;
+    int core_idx = 0;
     static uint64_t run_count = 0;
     val_t cpu_idle, cpu_usage, cpu_sys, cpu_user, cpu_iowait;
     char tmp[16], name[16];
@@ -75,32 +75,33 @@ int cpu_data_collect(item_t *data)
     p = update_file(&cpu_stat);
     mate_t *data_set = data->data;
     data->elememt_num = items[1].data_count;
-    for (;data_idx < data->elememt_num; data_idx++) {
-        cpu_idle.f = cpu_core_usage(data_idx, p);
+    for (;core_idx < (corenum + 1); core_idx++) {
+        cpu_idle.f = cpu_core_usage(core_idx, p);
         cpu_usage.f = 100.0 - cpu_idle.f;
-        cpu_user.f = core_user_usage(data_idx, p);
-        cpu_sys.f = core_sys_usage(data_idx, p);
-        cpu_iowait.f = core_iowait_usage(data_idx, p);
-        if (data_idx != 0) {
-            sprintf(tmp, "cpu%d", data_idx);
+        cpu_user.f = core_user_usage(core_idx, p);
+        cpu_sys.f = core_sys_usage(core_idx, p);
+        cpu_iowait.f = core_iowait_usage(core_idx, p);
+        if (core_idx != 0) {
+            sprintf(tmp, "cpu%d", core_idx);
         } else {
             sprintf(tmp, "cpu");
         }
         if (run_count != 0) {
+            int count = 0;
             sprintf(name, "%s usage", tmp);
-            set_data_collect(&(data_set[data_idx]), name, "%", M_FLOAT, cpu_usage);
+            set_data_collect(&(data_set[core_idx + count++]), name, "%", M_FLOAT, cpu_usage);
             //plugin_print_log("%s:%f\n", name, cpu_usage.f);
 
             sprintf(name, "%s user", tmp);
-            set_data_collect(&(data_set[data_idx]), name, "%", M_FLOAT, cpu_user);
+            set_data_collect(&(data_set[core_idx + count++]), name, "%", M_FLOAT, cpu_user);
             //plugin_print_log("%s:%f\n", name, cpu_user.f);
 
             sprintf(name, "%s system", tmp);
-            set_data_collect(&(data_set[data_idx]), name, "%", M_FLOAT, cpu_sys);
+            set_data_collect(&(data_set[core_idx + count++]), name, "%", M_FLOAT, cpu_sys);
             //plugin_print_log("%s:%f\n", name, cpu_sys.f);
 
             sprintf(name, "%s iowait", tmp);
-            set_data_collect(&(data_set[data_idx]), name, "%", M_FLOAT, cpu_iowait);
+            set_data_collect(&(data_set[core_idx + count++]), name, "%", M_FLOAT, cpu_iowait);
             //plugin_print_log("%s:%f\n", name, cpu_iowait.f);
         }
     }
@@ -114,7 +115,7 @@ PLUGIN_ENTRY(cpu, plugin_info)
     stat = update_file(&cpu_stat);
     corenum = get_core_num(stat);
     num_cpustates = num_cpustates_func(stat);
-    items[1].data_count = corenum + 1;
+    items[1].data_count = (corenum + 1) * 4;
 
     PLUGIN_INIT(plugin_info, items, &pluginfo);
     return 0;
